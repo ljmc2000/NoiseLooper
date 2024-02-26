1ee8e785dcc8e3c148b88e542c1d5a4a1f162528
Delilah <ljmc2000@gmail.com>
2024-02-26 21:29:47 +0000

    mock up UI

--- app/src/main/java/ie/delilahsthings/soothingloop/MainActivity.java
+++ app/src/main/java/ie/delilahsthings/soothingloop/MainActivity.java
@@ -1,14 +1,89 @@
 package ie.delilahsthings.soothingloop;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.recyclerview.widget.DividerItemDecoration;
 
+import android.content.res.Resources;
+import android.content.res.XmlResourceParser;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.ShapeDrawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private LinearLayout noise_list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        noise_list=this.findViewById(R.id.noise_list);
+
+        populateNoiselist();
+    }
+
+    void addDivider()
+    {
+        ViewGroup view = new LinearLayout(this);
+        View.inflate(this, R.layout.hline, view);
+        noise_list.addView(view);
+    }
+
+    void addHeader(String name)
+    {
+        TextView text = new TextView(this);
+        text.setText(name);
+        text.setGravity(Gravity.CENTER);
+        text.setTextSize(38);
+        text.setPadding(0,0,0,10);
+        noise_list.addView(text);
+    }
+
+    void addItem(String name)
+    {
+        ViewGroup view = new LinearLayout(this);
+        View.inflate(this, R.layout.noise_config_item, view);
+        noise_list.addView(view);
+
+        ((TextView) view.findViewById(R.id.noise_name)).setText(name);
+    }
+
+    void populateNoiselist()
+    {
+        addHeader("Anti Sound");
+        addItem("Brownian Noise");
+        addItem("Pink Noise");
+        addItem("White Noise");
+        addDivider();
+
+        addHeader("Nature");
+        addItem("Rain");
+        addItem("Storm");
+        addItem("Wind");
+        addItem("Waves");
+        addItem("Stream");
+        addItem("Birds");
+        addItem("Summer Night");
+        addDivider();
+
+        addHeader("Travel");
+        addItem("Train");
+        addItem("Boat");
+        addItem("City");
+        addDivider();
+
+        addHeader("Interiors");
+        addItem("Coffee Shop");
+        addItem("Fireplace");
+        addDivider();
     }
 }
