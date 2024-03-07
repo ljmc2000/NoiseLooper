89a50ed36c4f31b26533b7701bf57ebf43711784
Delilah McCormick <ljmc2000@gmail.com>
2024-03-07 00:01:53 +0000

    implemented restore state with default profile

--- app/src/main/java/ie/delilahsthings/soothingloop/MainActivity.java
+++ app/src/main/java/ie/delilahsthings/soothingloop/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.media.AudioManager;
 import android.media.SoundPool;
@@ -21,10 +22,12 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.prefs.Preferences;
 
 public class MainActivity extends AppCompatActivity {
 
     private AudioManager audioManager;
+    SharedPreferences defaultPref;
     private LinearLayout noise_list;
     private Resources resources;
     private SoundPool soundPool;
@@ -41,9 +44,22 @@ public class MainActivity extends AppCompatActivity {
         resources=getResources();
 
         audioManager=(AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        defaultPref=getSharedPreferences("default",MODE_PRIVATE);
         soundPool=new SoundPool(32, AudioManager.STREAM_MUSIC, 0);
 
         populateNoiselist();
+    }
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        try {
+            Thread.sleep(1000);
+            loadDefaults();
+        }
+        catch (InterruptedException e){
+        }
     }
 
     @Override
@@ -71,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
         noise_list.addView(text);
     }
 
-    void addItem(int iconId, int nameId, int soundId)
+    void addItem(int iconId, int nameId, int soundId, String persistKey)
     {
         ViewGroup view = new LinearLayout(this);
         View.inflate(this, R.layout.noise_config_item, view);
@@ -85,36 +101,37 @@ public class MainActivity extends AppCompatActivity {
         SoundEffectVolumeManager manager=new SoundEffectVolumeManager(getBaseContext(),soundPool,soundId);
         managers.add(manager);
         volume.setOnSeekBarChangeListener(manager);
+        volume.setTag(R.string.persist_key,persistKey);
     }
 
     void populateNoiselist()
     {
         addHeader(resources.getString(R.string.header_antisound));
-        //addItem(R.drawable.brown_noise,R.string.brown_noise,R.raw.brown_noise);   //unapproved upstream, for personal use
-        addItem(R.drawable.pink_noise,R.string.pink_noise,R.raw.pink_noise);
-        addItem(R.drawable.white_noise,R.string.white_noise,R.raw.white_noise);
+        //addItem(R.drawable.brown_noise,R.string.brown_noise,R.raw.brown_noise,"brown_noise");   //unapproved upstream, for personal use
+        addItem(R.drawable.pink_noise,R.string.pink_noise,R.raw.pink_noise,"pink_noise");
+        addItem(R.drawable.white_noise,R.string.white_noise,R.raw.white_noise, "white_noise");
         addDivider();
 
         addHeader(resources.getString(R.string.header_nature));
-        addItem(R.drawable.rain,R.string.rain,R.raw.rain);
-        addItem(R.drawable.storm,R.string.storm,R.raw.storm);
-        addItem(R.drawable.wind,R.string.wind,R.raw.wind);
-        addItem(R.drawable.waves,R.string.waves,R.raw.waves);
-        addItem(R.drawable.stream,R.string.stream,R.raw.stream);
-        addItem(R.drawable.birds,R.string.birds,R.raw.birds);
-        addItem(R.drawable.summer_night,R.string.summer_night,R.raw.summer_night);
+        addItem(R.drawable.rain,R.string.rain,R.raw.rain,"rain");
+        addItem(R.drawable.storm,R.string.storm,R.raw.storm,"storm");
+        addItem(R.drawable.wind,R.string.wind,R.raw.wind,"wind");
+        addItem(R.drawable.waves,R.string.waves,R.raw.waves,"waves");
+        addItem(R.drawable.stream,R.string.stream,R.raw.stream,"stream");
+        addItem(R.drawable.birds,R.string.birds,R.raw.birds,"birds");
+        addItem(R.drawable.summer_night,R.string.summer_night,R.raw.summer_night,"summer_night");
 
         addDivider();
 
         addHeader(resources.getString(R.string.header_travel));
-        addItem(R.drawable.train,R.string.train,R.raw.train);
-        addItem(R.drawable.boat,R.string.boat,R.raw.boat);
-        addItem(R.drawable.city,R.string.city,R.raw.city);
+        addItem(R.drawable.train,R.string.train,R.raw.train,"train");
+        addItem(R.drawable.boat,R.string.boat,R.raw.boat,"boat");
+        addItem(R.drawable.city,R.string.city,R.raw.city,"city");
         addDivider();
 
         addHeader(resources.getString(R.string.header_interiors));
-        addItem(R.drawable.coffee_shop,R.string.coffee_shop,R.raw.coffee_shop);
-        addItem(R.drawable.fireplace,R.string.fireplace,R.raw.fireplace);
+        addItem(R.drawable.coffee_shop,R.string.coffee_shop,R.raw.coffee_shop,"coffee_shop");
+        addItem(R.drawable.fireplace,R.string.fireplace,R.raw.fireplace,"fireplace");
         addDivider();
     }
 
@@ -122,6 +139,45 @@ public class MainActivity extends AppCompatActivity {
     {
         Intent showCredits = new Intent(this, CreditsActivity.class);
         startActivity(showCredits);
+    }
+
+    public void loadDefaults(MenuItem sender){
+        loadDefaults();
+    }
+
+    private void loadDefaults()
+    {
+        SeekBar v;
+        String persistKey;
+
+        for(int i=0; i<noise_list.getChildCount(); i++)
+        {
+            v=noise_list.getChildAt(i).findViewById(R.id.volume);
+            if(v!=null)
+            {
+                persistKey=(String) v.getTag(R.string.persist_key);
+                v.setProgress(defaultPref.getInt(persistKey,0));
+            }
+        }
+    }
+
+    public void saveDefaults(MenuItem sender)
+    {
+        SeekBar v;
+        String persistKey;
+        SharedPreferences.Editor editor = defaultPref.edit();
+
+        for(int i=0; i<noise_list.getChildCount(); i++)
+        {
+            v=noise_list.getChildAt(i).findViewById(R.id.volume);
+            if(v!=null)
+            {
+                persistKey=(String) v.getTag(R.string.persist_key);
+                editor.putInt(persistKey,v.getProgress());
+            }
+        }
+
+        editor.commit();
     }
 
     public void silenceAll(MenuItem sender)
