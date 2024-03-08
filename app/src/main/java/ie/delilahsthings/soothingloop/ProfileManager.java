d4641ec48bec3c482f5c1ae4cb4112dd8a64527f
Delilah McCormick <ljmc2000@gmail.com>
2024-03-08 23:41:49 +0000

    can now add and remove custom sounds from settings activity

--- app/src/main/java/ie/delilahsthings/soothingloop/ProfileManager.java
+++ app/src/main/java/ie/delilahsthings/soothingloop/ProfileManager.java
@@ -1,9 +1,22 @@
 package ie.delilahsthings.soothingloop;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Environment;
+import android.provider.OpenableColumns;
+import android.util.Log;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 
 public abstract class ProfileManager {
@@ -12,10 +25,30 @@ public abstract class ProfileManager {
     final public static String suffix=".xml";
     final public static int suffix_length=suffix.length();
 
+    final private static String CUSTOM_SOUNDS = "/custom_sounds/";
+    final private static String DATA = "/data/";
+    final private static String SHARED_PREFS = "/shared_prefs/";
+
+    static String getProfilePath(Context context)
+    {
+        return Environment.getDataDirectory().getPath()+DATA+context.getPackageName()+SHARED_PREFS;
+    }
+
+    static String getSoundPath(Context context)
+    {
+        return Environment.getDataDirectory().getPath()+DATA+context.getPackageName()+CUSTOM_SOUNDS;
+    }
+
+    public static boolean deleteProfile(Context context, String profile)
+    {
+        String path = getProfilePath(context)+prefix+profile+suffix;
+        return new File(path).delete();
+    }
+
     public static String[] listProfiles(Context context)
     {
         ArrayList<String> files = new ArrayList();
-        String path = Environment.getDataDirectory().getPath()+"/data/"+context.getPackageName()+"/shared_prefs";
+        String path = getProfilePath(context);
         for(String file: new File(path).list()) {
             if (file.startsWith(ProfileManager.prefix))
                 files.add(file.substring(ProfileManager.prefix_length, file.length() - ProfileManager.suffix_length));
@@ -23,9 +56,63 @@ public abstract class ProfileManager {
         return files.toArray(new String[0]);
     }
 
-    public static boolean deleteProfile(Context context, String profile)
+    public static String addCustomSound(Context context, Uri uri) throws IOException {
+        String path = getSoundPath(context);
+        File dir = new File(path);
+        dir.mkdirs();
+
+        InputStream inputStream = context.getContentResolver().openInputStream(uri);
+        File dest = new File(path+getFileName(context,uri));
+        dest.createNewFile();
+        copy(inputStream,dest);
+        return dest.getName();
+    }
+
+    public static boolean deleteCustomSound(Context context, String sound)
     {
-        String path = Environment.getDataDirectory().getPath()+"/data/"+context.getPackageName()+"/shared_prefs/"+prefix+profile+suffix;
+        String path = getSoundPath(context)+sound;
         return new File(path).delete();
+    }
+
+    public static String[] listCustomSounds(Context context)
+    {
+        String path = getSoundPath(context);
+        File dir = new File(path);
+        dir.mkdirs();
+        return dir.list();
+    }
+
+    static void copy(InputStream in, File dst) throws IOException {
+        try (OutputStream out = new FileOutputStream(dst)) {
+            // Transfer bytes from in to out
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = in.read(buf)) > 0) {
+                out.write(buf, 0, len);
+            }
+        }
+    }
+
+    @SuppressLint("Range")
+    public static String getFileName(Context context, Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            Cursor cursor = context.getContentResolver().query(uri, null, null, null, null);
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        if (result == null) {
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
     }
 }
--- app/src/main/java/ie/delilahsthings/soothingloop/SettingsActivity.java
+++ app/src/main/java/ie/delilahsthings/soothingloop/SettingsActivity.java
@@ -1,25 +1,55 @@
 package ie.delilahsthings.soothingloop;
 
 import android.app.AlertDialog;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
 
 public class SettingsActivity extends AppCompatActivity {
 
-    ViewGroup profileList;
+    private ViewGroup profilesView;
+    private ViewGroup customSoundsView;
+    private ActivityResultLauncher<String> getNewSound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        this.profileList=findViewById(R.id.profiles);
+        this.profilesView=findViewById(R.id.profiles);
+        this.customSoundsView=findViewById(R.id.custom_sounds);
+        this.getNewSound=registerForActivityResult(new ActivityResultContracts.GetContent(), (uri)->addCustomSound(uri));
         populateCustomProfiles();
+        populateCustomSounds();
+    }
+
+    void addCustomSound(Uri uri)
+    {
+        try {
+            String sound=ProfileManager.addCustomSound(this,uri);
+            ViewGroup view = new LinearLayout(this);
+            View.inflate(this, R.layout.profile_config_item, view);
+            TextView text=view.findViewById(R.id.title);
+            text.setText(sound);
+            ImageView image=view.findViewById(R.id.delete_button);
+            image.setOnClickListener((v)->promptDeleteCustomSound(view, sound));
+
+            customSoundsView.addView(view);
+        }
+        catch (IOException e)
+        {
+            Toast.makeText(this,getString(R.string.add_sound_problem),Toast.LENGTH_SHORT).show();
+        }
     }
 
     void populateCustomProfiles()
@@ -35,8 +65,47 @@ public class SettingsActivity extends AppCompatActivity {
             image=view.findViewById(R.id.delete_button);
             image.setOnClickListener((v)->promptDeleteProfile(view, profileName));
 
-            profileList.addView(view);
+            profilesView.addView(view);
         }
+    }
+
+    void populateCustomSounds()
+    {
+        TextView text;
+        ImageView image;
+
+        for(String sound: ProfileManager.listCustomSounds(this))
+        {
+            ViewGroup view = new LinearLayout(this);
+            View.inflate(this, R.layout.profile_config_item, view);
+            text=view.findViewById(R.id.title);
+            text.setText(sound);
+            image=view.findViewById(R.id.delete_button);
+            image.setOnClickListener((v)->promptDeleteCustomSound(view, sound));
+
+            customSoundsView.addView(view);
+        }
+    }
+
+    public void promptAddCustomSound(View view)
+    {
+        getNewSound.launch("audio/*");
+    }
+
+    public void promptDeleteCustomSound(View sender, String sound)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(String.format(getString(R.string.confirm_delete_custom_sound),sound));
+        builder.setNegativeButton(R.string.cancel, (dialogInterface, i) -> dialogInterface.cancel());
+        builder.setPositiveButton(getString(R.string.confirm),(dialogInterface, i) -> {
+            ProfileManager.deleteCustomSound(this,sound);
+            customSoundsView.removeView(sender);
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+        Workarounds.fixBadDialogButtonColours(this, dialog);
     }
 
     void promptDeleteProfile(View sender, String profileName)
@@ -46,7 +115,7 @@ public class SettingsActivity extends AppCompatActivity {
         builder.setNegativeButton(R.string.cancel, (dialogInterface, i) -> dialogInterface.cancel());
         builder.setPositiveButton(getString(R.string.confirm),(dialogInterface, i) -> {
             ProfileManager.deleteProfile(this,profileName);
-            profileList.removeView(sender);
+            profilesView.removeView(sender);
         });
 
         AlertDialog dialog = builder.create();
