677c407dc78e61d3138194da2e04aa910a0f599d
Delilah McCormick <ljmc2000@gmail.com>
2024-03-22 22:02:04 +0000

    the sleep timer is now accurate to the system clock

--- app/src/main/java/ie/delilahsthings/soothingloop/SleepTimerThread.java
+++ app/src/main/java/ie/delilahsthings/soothingloop/SleepTimerThread.java
@@ -7,46 +7,48 @@ public class SleepTimerThread  extends Thread{
     static SleepTimerThread singleton = null;
 
     public static volatile Context context;
-    private volatile long remainingTime=-1;
+    private volatile long endTime=-1;
 
     private SleepTimerThread()
     {
     }
 
-    private synchronized void updateTimer()
+    private synchronized long getRemainingTime()
     {
-        remainingTime--;
+        return endTime-System.currentTimeMillis();
     }
 
     @Override
     public void run()
     {
         Intent intent;
+        long remainingTime;
 
-        while(remainingTime>=0) {
+        do {
+            remainingTime = getRemainingTime();
             if (context != null) {
                 intent = new Intent(Constants.TIMER_EVENT);
-                intent.putExtra(Constants.REMAINING_TIME, remainingTime);
+                intent.putExtra(Constants.REMAINING_TIME, remainingTime/1000);
                 intent.setPackage(context.getPackageName());
                 context.sendBroadcast(intent);
             }
 
-            updateTimer();
-            Util.sleep(1000);
-        }
+            Util.sleep(500);
+
+        } while(remainingTime>=0);
 
         singleton=null;
     }
 
-    public static synchronized void setTime(long timeout)
+    public static synchronized void setTime(long timeoutInSeconds)
     {
         if(singleton==null) {
             singleton = new SleepTimerThread();
-            singleton.remainingTime=timeout;
+            singleton.endTime=System.currentTimeMillis()+(1000*timeoutInSeconds);
             singleton.start();
         }
         else {
-            singleton.remainingTime=timeout;
+            singleton.endTime=System.currentTimeMillis()+(1000*timeoutInSeconds);
         }
     }
 
