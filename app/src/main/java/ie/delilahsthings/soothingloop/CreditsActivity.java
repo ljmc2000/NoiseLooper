7959050bb7caa5d140da2626de77bf12793eae08
Delilah McCormick <ljmc2000@gmail.com>
2024-03-25 00:22:53 +0000

    credits now html based. includes links to all sounds

--- app/src/main/java/ie/delilahsthings/soothingloop/CreditsActivity.java
+++ app/src/main/java/ie/delilahsthings/soothingloop/CreditsActivity.java
@@ -1,7 +1,7 @@
 package ie.delilahsthings.soothingloop;
 
 import android.os.Bundle;
-import android.widget.TextView;
+import android.webkit.WebView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,10 +12,18 @@ import org.xml.sax.SAXException;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringWriter;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import static ie.delilahsthings.soothingloop.HtmlConstants.*;
 
 public class CreditsActivity extends AppCompatActivity {
 
@@ -23,72 +31,132 @@ public class CreditsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
-        TextView creditsView = this.findViewById(R.id.sound_credits);
+        WebView creditsView = this.findViewById(R.id.sound_credits);
+        creditsView.setBackgroundColor(getResources().getColor(R.color.bg_color_light));
 
         try {
-            creditsView.setText(loadCredits());
+            creditsView.loadData(loadCredits(),"text/html","utf8");
         } catch (Exception e) {
-            creditsView.setText(getString(R.string.credits_error));
+            creditsView.loadData(getString(R.string.credits_error),"text/plain","utf8");
             //throw new RuntimeException(e);
         }
     }
 
     private String loadCredits() throws ParserConfigurationException, IOException, SAXException {
         InputStream creditsFile = getResources().openRawResource(R.raw.credits);
-        String credits = "";
-        String header = getString(R.string.credits_header);
-        String Tab="    ";
-        String Tab2="        ";
+        InputStream creditsTemplate = getResources().openRawResource(R.raw.credits_template);
+        String HEADER = getString(R.string.credits_header);
 
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         DocumentBuilder db = dbf.newDocumentBuilder();
         Document document = db.parse(creditsFile);
         Element node;
 
+        Document credits = db.parse(creditsTemplate);
+        Element header, body, item;
+
+        header = credits.getElementById("page_header");
+        header.setTextContent(getString(R.string.credits_label));
+
         NodeList developerCredits=document.getElementsByTagName("developer");
-        credits+=String.format(header,getString(R.string.developers))+"\n";
+        header=credits.getElementById("developer_credits_header");
+        header.setTextContent(String.format(HEADER,getString(R.string.developers)));
+        body=credits.getElementById("developer_credits");
         for(int i=0; i<developerCredits.getLength(); i++) {
             node=(Element) developerCredits.item(i);
-            credits+=Tab+node.getTextContent()+"\n";
+            developer(credits,body,node);
         }
-        credits+="\n";
+
 
         NodeList upstreamCredits=document.getElementsByTagName("upstream_developer");
-        credits+=String.format(header,getString(R.string.upstream_application))+"\n";
+        header=credits.getElementById("upstream_application_credits_header");
+        header.setTextContent(String.format(HEADER,getString(R.string.upstream_application)));
+        body=credits.getElementById("upstream_application_credits");
         for(int i=0; i<upstreamCredits.getLength(); i++) {
             node=(Element) upstreamCredits.item(i);
-            credits+=Tab+node.getTextContent()+"\n";
+            developer(credits,body,node);
         }
-        credits+="\n";
 
-        String Author = String.format(header,getString(R.string.author));
-        String Editor = String.format(header,getString(R.string.editor));
-        String Icon = String.format(header,getString(R.string.icon));
-        String License = String.format(header,getString(R.string.license));
+
+
+        String Author = String.format(HEADER,getString(R.string.author));
+        String Editor = String.format(HEADER,getString(R.string.editor));
+        String Icon = String.format(HEADER,getString(R.string.icon));
+        String License = String.format(HEADER,getString(R.string.license));
         String author, editor, icon, license;
 
-        credits+=String.format(header,getString(R.string.sounds))+"\n";
+        header=credits.getElementById("sounds_credits_header");
+        header.setTextContent(String.format(HEADER,getString(R.string.sounds)));
+        body=credits.getElementById("sounds_credits");
         NodeList sounds = document.getElementsByTagName("sound");
 
         int nameId;
         for(int i=0; i<sounds.getLength(); i++) {
             node = (Element) sounds.item(i);
             nameId = getResources().getIdentifier(node.getAttribute("id"), "string", getPackageName());
-            credits += (Tab + String.format(header, getString(nameId)) + "\n");
+
             author = node.getAttribute("author");
             editor = node.getAttribute("editor");
             icon = node.getAttribute("icon");
             license = node.getAttribute("license");
+
+            item=credits.createElement(A);
+            item.setTextContent(getString(nameId));
+            item.setAttribute(HREF,node.getAttribute("source"));
+            body.appendChild(item);
+
             if (!author.isEmpty())
-                credits += (Tab2 + Author + author + "\n");
+                doubleIndent(credits, body, Author+author);
             if (!editor.isEmpty())
-                credits += (Tab2 + Editor + editor + "\n");
+                doubleIndent(credits, body, Editor+editor);
             if (!icon.isEmpty())
-                credits += (Tab2 + Icon + icon + "\n");
+                doubleIndent(credits, body, Icon+icon);
             if (!license.isEmpty())
-                credits += (Tab2 + License + license + "\n");
+                doubleIndent(credits, body, License+license);
         }
 
-        return credits;
+        return getStringFromDocument(credits);
+    }
+
+    private void developer(Document credits, Element parent, Element node) {
+        String website = node.getAttribute("website");
+        Element item, subitem;
+
+        if(!website.isEmpty()) {
+            item = credits.createElement(DIV);
+            subitem = credits.createElement(A);
+            subitem.setTextContent(node.getTextContent());
+            subitem.setAttribute(HREF, website);
+            item.appendChild(subitem);
+            parent.appendChild(item);
+        }
+
+        else {
+            item = credits.createElement(DIV);
+            item.setTextContent(node.getTextContent());
+            parent.appendChild(item);
+        }
+    }
+
+    void doubleIndent(Document credits, Element parent, String content) {
+        Element e = credits.createElement(DIV);
+        e.setTextContent(content);
+        e.setAttribute("class", "double_indented_block");
+        parent.appendChild(e);
+    }
+
+    public static String getStringFromDocument(Document doc) {
+        try {
+            DOMSource domSource = new DOMSource(doc);
+            StringWriter writer = new StringWriter();
+            StreamResult result = new StreamResult(writer);
+            TransformerFactory tf = TransformerFactory.newInstance();
+            Transformer transformer = tf.newTransformer();
+            transformer.transform(domSource, result);
+            return writer.toString();
+        }
+        catch (TransformerException ex) {
+            return "";
+        }
     }
 }
---
+++ app/src/main/java/ie/delilahsthings/soothingloop/HtmlConstants.java
@@ -0,0 +1,7 @@
+package ie.delilahsthings.soothingloop;
+
+public abstract class HtmlConstants {
+    static String A = "a";
+    static String DIV = "div";
+    static String HREF = "href";
+}
